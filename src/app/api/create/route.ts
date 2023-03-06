c7c69d0beb2e3122eda4cc203266bb6b36bf58f4
Marcelo Arias <hello@marceloarias.com>
2023-03-06 10:51:18 -0500

    Add TODOs for /api/create

--- src/app/api/create/route.ts
+++ src/app/api/create/route.ts
@@ -11,6 +11,11 @@ type RequestBody = {
  */
 export async function POST(request: NextRequest) {
   const body: RequestBody = await request.json();
+  const prompt: string = body.prompt;
+
+  // TODO: Verify that prompt is not empty and that is less than 42 characters.
+
+  // TODO: Allow 6 generated images per IP per day.
 
   const response = await fetch("https://api.replicate.com/v1/predictions", {
     method: "POST",
@@ -25,7 +30,7 @@ export async function POST(request: NextRequest) {
         "6359a0cab3ca6e4d3320c33d79096161208e9024d174b2311e5a21b6c7e1131c",
 
       // This is the text prompt that will be submitted by a form on the frontend
-      input: { prompt: body.prompt },
+      input: { prompt },
     }),
   });
 

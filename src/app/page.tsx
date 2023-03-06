4ee2ffe48c85d120ae082c288398b74a228e6e1a
Marcelo Arias <hello@marceloarias.com>
2023-03-06 10:51:34 -0500

    Add maxLength to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,6 +66,7 @@ export default function Home() {
           className="flex h-10 w-full rounded-md border border-slate-300 bg-transparent py-2 px-3 placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-700 dark:text-slate-50 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900"
           value={prompt}
           onChange={(e) => setPrompt(e.target.value)}
+          maxLength={42}
         />
         <button
           type="submit"

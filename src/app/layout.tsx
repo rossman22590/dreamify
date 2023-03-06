6b245e0453933ab3fbb36ae0a4a16ce777365c5a
Marcelo Arias <hello@marceloarias.com>
2023-03-06 10:11:59 -0500

    Update information of metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,26 @@
-import './globals.css'
+import { Inter } from "next/font/google";
+import "./globals.css";
 
 /**
+ * @name metadata
+ * @description Metadata for the page.
  * @link https://beta.nextjs.org/docs/api-reference/file-conventions/head
  */
 export const metadata = {
-  title: 'Dreamify',
-  description: 'A simple generator of images from AI. Using Stable Diffusion.',
-}
+  title: "Dreamify",
+  description: "A simple generator of images from AI. Using Stable Diffusion. Built on Next.js 13.2",
+};
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>{children}</body>
     </html>
-  )
+  );
 }

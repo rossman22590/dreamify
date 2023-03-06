4f4799c1081637ff1a0726e8888b686e7a250e16
Marcelo Arias <hello@marceloarias.com>
2023-03-05 23:43:42 -0500

    Set Replicate endpoint for Next configuration file

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "replicate.com",
+      },
+      {
+        protocol: "https",
+        hostname: "replicate.delivery",
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig

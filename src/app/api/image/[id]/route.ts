f7c612a553df3fb4d98fd6bfbfc7e793b1712514
Marcelo Arias <hello@marceloarias.com>
2023-03-06 10:44:30 -0500

    Add GET /api/image/:id

---
+++ src/app/api/image/[id]/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+
+/**
+ * @name GET /api/image/:id
+ * @summary Get an image from Replicate API of Stable Diffusion by ID
+ * @param request {NextRequest}
+ */
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const id = params.id;
+
+  const response = await fetch(
+    "https://api.replicate.com/v1/predictions/" + id,
+    {
+      headers: {
+        Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  if (response.status !== 200) {
+    let error = await response.json();
+    return NextResponse.json({ detail: error.detail }, { status: 500 });
+  }
+
+  const prediction = await response.json();
+  return NextResponse.json({ ...prediction }, { status: 200 });
+}

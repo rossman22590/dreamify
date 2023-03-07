f97bdd33e2e588a8e3d27f24a82e201056bc360c
Marcelo Arias <hello@marceloarias.com>
2023-03-06 21:08:38 -0500

    Add header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Image as ImageIcon } from "lucide-react";
+import Link from "next/link";
 
 /**
  * @name metadata
@@ -21,7 +23,55 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className}>
-      <body>{children}</body>
+      <body>
+        <header className="flex flex-row items-center gap-x-4 justify-center">
+          <Link href="/" className="flex flex-row items-center gap-x-4">
+            <ImageIcon className="h-10 w-10 text-blue-600 dark:text-blue-400" />
+            <h1 className="text-4xl leading-normal text-center my-8 font-bold text-transparent bg-clip-text bg-gradient-to-br from-blue-600 to-blue-700 select-none dark:from-blue-400 dark:to-blue-400">
+              Dreamify
+            </h1>
+          </Link>
+        </header>
+        {children}
+        <footer className="bg-white md:flex dark:bg-black mt-12 border border-t-slate-600 border-transparent">
+          <div className="m-auto max-w-4xl md:py-4 flex w-full flex-col md:flex-row md:justify-between">
+            <div className="">
+              <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                © 2023 Dreamify. Created by{" "}
+                <a
+                  href="https://github.com/360macky"
+                  target="_blank"
+                  className="hover:underline"
+                >
+                  Marcelo Arias
+                </a>
+              </span>
+            </div>
+            <div className="">
+              <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+                <li>
+                  <a
+                    href="https://gitub.com/"
+                    target="_blank"
+                    className="mr-4 hover:underline md:mr-6 "
+                  >
+                    Repository
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://gitub.com/"
+                    target="_blank"
+                    className="mr-4 hover:underline md:mr-6"
+                  >
+                    License
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </footer>
+      </body>
     </html>
   );
 }

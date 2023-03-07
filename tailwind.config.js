3b6021baeaba5ede7a77cf5d2bab5e5686a81e4a
Marcelo Arias <hello@marceloarias.com>
2023-03-07 16:59:28 -0500

    Add animations to Tailwind configuration file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
   plugins: [],
 };

eefeebc5c3b184eab1c12046b65d9fe8af971398
Marcelo Arias <hello@marceloarias.com>
2023-03-07 17:13:37 -0500

    Add limit to roue

--- src/app/api/create/route.ts
+++ src/app/api/create/route.ts
@@ -1,8 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
+import rateLimit from "@/utils/rate-limit";
 
 type RequestBody = {
   prompt: string;
 };
+
+// 4 requests per hour
+const REQUESTS_PER_INTERVAL = 2;
+const INTERVAL = 60 * 1000 * 60;
+const limiter = rateLimit({ interval: INTERVAL });
 
 /**
  * @name POST /api/create
@@ -24,10 +30,31 @@ export async function POST(request: NextRequest) {
     );
   }
 
-  // TODO: Allow 6 generated images per IP per day.
-
   // TODO: Create a cancel prediction endpoint that allows the user to cancel a prediction.
   // https://replicate.com/docs/reference/http#cancel-prediction
+
+  const { isLimitExceeded, responseHeaders } = limiter.check(
+    REQUESTS_PER_INTERVAL
+  );
+
+  const getCurrentHourPlusOne = () => {
+    const date = new Date();
+    const hour = date.getHours() + 1;
+    const minutes = date.getMinutes();
+    const ampm = hour >= 12 ? "PM" : "AM";
+    const hour12 = hour % 12 || 12;
+    const minutesFormatted = minutes < 10 ? `0${minutes}` : minutes;
+    return `${hour12}:${minutesFormatted} ${ampm}`;
+  };
+
+  if (isLimitExceeded) {
+    return NextResponse.json(
+      {
+        detail: `Rate limit exceeded. Please try again later in 1 hour (around ${getCurrentHourPlusOne()}).`,
+      },
+      { status: 429, headers: responseHeaders }
+    );
+  }
 
   const response = await fetch("https://api.replicate.com/v1/predictions", {
     method: "POST",
@@ -48,9 +75,15 @@ export async function POST(request: NextRequest) {
 
   if (response.status !== 201) {
     let error = await response.json();
-    return NextResponse.json({ detail: error.detail }, { status: 500 });
+    return NextResponse.json(
+      { detail: error.detail },
+      { status: 500, headers: responseHeaders }
+    );
   }
 
   const prediction = await response.json();
-  return NextResponse.json({ ...prediction }, { status: 201 });
+  return NextResponse.json(
+    { ...prediction },
+    { status: 201, headers: responseHeaders }
+  );
 }

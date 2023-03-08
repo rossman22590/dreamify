4bf5345d4bcc2faa91df5219ec05dec08f0e2c78
Marcelo Arias <hello@marceloarias.com>
2023-03-07 20:22:43 -0500

    Add percentage and share button for mobile

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 import { useState } from "react";
 import { Button } from "@/components/ui/Button";
 import { Input } from "@/components/ui/Input";
-import { Download, Flower2, Loader2, Wand2 } from "lucide-react";
+import { Progress } from "@/components/ui/progress";
+import { Download, Flower2, Loader2, Wand2, Share } from "lucide-react";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -31,6 +32,7 @@ export default function Page() {
   const [predictionStatus, setPredictionStatus] =
     useState<PredictionStatus>("initial");
   const [isAlertOpen, setIsAlertOpen] = useState(false);
+  const [percentage, setPercentage] = useState<number>(0);
 
   const replaceSpacesWithDashes = (str: string) => {
     return str.replace(/\s+/g, "-").toLowerCase();
@@ -48,6 +50,40 @@ export default function Page() {
     a.href = window.URL.createObjectURL(blob);
     a.setAttribute("download", `${replaceSpacesWithDashes(prompt)}.png`);
     a.click();
+  };
+
+  const getLatestPercentage = (logs: any): number => {
+    const regex = /(\d+)%\|.*?$/gm;
+    let match;
+    let lastPercentage;
+    while ((match = regex.exec(logs))) {
+      lastPercentage = match[1];
+    }
+    return Number(lastPercentage);
+  };
+
+  const isUserOnMobile =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
+
+  const handleShare = async () => {
+    if (isUserOnMobile) {
+      const blob = await fetch(imageUrl).then((r) => r.blob());
+      const filesArray = [
+        new File([blob], `${replaceSpacesWithDashes(prompt)}.png`, {
+          type: "image/jpeg",
+          lastModified: new Date().getTime(),
+        }),
+      ];
+      navigator.share({
+        title: `${prompt} - dreamify.art`,
+        text: `${prompt} - dreamify.art`,
+        files: filesArray,
+      });
+    } else {
+      handleDownloadImage();
+    }
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -85,6 +121,7 @@ export default function Page() {
       await sleep(1000);
       const response = await fetch("/api/image/" + prediction.id);
       prediction = await response.json();
+      setPercentage(getLatestPercentage(prediction.logs));
       if (response.status !== 200) {
         setError(prediction.detail);
         setPredictionStatus("error");
@@ -111,14 +148,19 @@ export default function Page() {
           value={prompt}
           onChange={(e) => setPrompt(e.target.value)}
           maxLength={42}
+          disabled={predictionStatus === "loading"}
         />
         <Button type="submit">
           <Wand2 className="mr-2 h-5 w-5" />
-          Generate
+          {predictionStatus === "loading" ? "Generating" : "Generate"}
         </Button>
       </form>
 
-      <div className="m-auto max-w-lg mt-3 lg:mt-6">
+      <div className="m-auto max-w-lg mt-3 lg:mt-3">
+        <Progress value={percentage} />
+      </div>
+
+      <div className="m-auto max-w-lg mt-3 lg:mt-3">
         <div>
           <div className="w-full relative aspect-square rounded bg-slate-100 dark:bg-slate-900 flex flex-col justify-center items-center gap-y-4">
             {prediction && prediction.output ? (
@@ -154,18 +196,30 @@ export default function Page() {
               </>
             )}
           </div>
-          <div>
-            <div className="flex py-2">
-              <Button
-                onClick={handleDownloadImage}
-                variant={"outline"}
-                className="w-full"
-              >
-                <Download className="mr-2 h-5 w-5" />
-                Download image
-              </Button>
+          {predictionStatus === "succeeded" && (
+            <div>
+              <div className="flex flex-col lg:flex-row gap-y-2 py-2 gap-x-2">
+                <Button
+                  onClick={handleDownloadImage}
+                  variant={"outline"}
+                  className="w-full"
+                >
+                  <Download className="mr-2 h-5 w-5" />
+                  Download image
+                </Button>
+                {isUserOnMobile && (
+                  <Button
+                    onClick={handleShare}
+                    variant={"outline"}
+                    className="w-full"
+                  >
+                    <Share className="mr-2 h-5 w-5" />
+                    Share
+                  </Button>
+                )}
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
       <AlertDialog onOpenChange={setIsAlertOpen} open={isAlertOpen}>
